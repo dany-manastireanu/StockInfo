62e22c67dea60b832a946cc39df1653dccebec16
Dany Manastireanu <danym@microsoft.com>
2022-12-01 19:18:56 +0200

    Using prices

--- data-collector-c2/main.js
+++ data-collector-c2/main.js
@@ -1,3 +1,43 @@
-// Extract data from opcom
-// Create an array of prices
-// Send data to ServiceBus using "send-message.js"
+const { ServiceBusClient } = require("@azure/service-bus");
+
+const connectionString = "Endpoint=sb://stockinfo.servicebus.windows.net/;SharedAccessKeyName=all;SharedAccessKey=2ypeUYjs322rbphXb62BlWqsvscu2y3nWYK3uSCul7Y=;EntityPath=primary-queue";
+const queueName = "primary-queue";
+
+
+async function sendMessage(message) {
+    const sbClient = new ServiceBusClient(connectionString);
+    try {
+        const sender = sbClient.createSender(queueName);
+    
+        console.log(`Sending all prices for next day`);
+
+        await sender.sendMessages(message);
+    
+        console.log(`Done sending, closing...`);
+        await sender.close();
+    } finally {
+        sbClient.close();
+    }
+}
+
+async function main() {
+    // Parse and get the prices + date + h
+
+    const prices = Array.from({length: 12}, (_, __) => ({
+        "date": "2022-01-01-22",
+        "price": Math.random() * 100
+    }));
+
+    const message = {
+        contentType: "application/json",
+        subject: "StockPrice",
+        body: prices
+    };
+    
+    await sendMessage(message);
+}
+
+main().catch((err) => {
+    console.log("Data Collector - Error occurred: ", err);
+    process.exit(1);
+});
--- data-sender-c4/main.js
+++ data-sender-c4/main.js
@@ -5,48 +5,50 @@ const queueName = "primary-queue";
 
 
 async function receiveMessages(numberOfMessages) {
-    const sbClient = new ServiceBusClient(connectionString);
-    const queueReceiver = sbClient.createReceiver(queueName);
-  
-    let allMessages = [];
+  const sbClient = new ServiceBusClient(connectionString);
+  const queueReceiver = sbClient.createReceiver(queueName);
 
-    try {
-  
-      console.log(`Receiving ${numberOfMessages} messages...`);
-  
-      while (allMessages.length < 1) {
-        // NOTE: asking for 10 messages does not guarantee that we will return
-        // all 10 at once so we must loop until we get all the messages we expected.
-        const messages = await queueReceiver.receiveMessages(10, {
-          maxWaitTimeInMs: 6 * 1000,
-        });
-  
-        if (!messages.length) {
-          console.log("No more messages to receive");
-          break;
-        }
-  
-        console.log(`Received ${messages.length} messages`);
-        allMessages.push(...messages);
-  
-        for (let message of messages) {
-          console.log(`  Message: '${JSON.stringify(message.body)}'`);
-  
-          // completing the message will remove it from the remote queue or subscription.
-          await queueReceiver.completeMessage(message);
-        }
+  let allMessages = [];
+
+  try {
+
+    console.log(`Receiving ${numberOfMessages} messages...`);
+
+    while (allMessages.length < 1) {
+      // NOTE: asking for 10 messages does not guarantee that we will return
+      // all 10 at once so we must loop until we get all the messages we expected.
+      const messages = await queueReceiver.receiveMessages(10, {
+        maxWaitTimeInMs: 6 * 1000,
+      });
+
+      if (!messages.length) {
+        console.log("No more messages to receive");
+        break;
       }
-  
-      await queueReceiver.close();
-    } finally {
-      await sbClient.close();
+
+      console.log(`Received ${messages.length} messages`);
+      allMessages.push(...messages);
+
+      for (let message of messages) {
+        console.log(`  Message: '${JSON.stringify(message.body)}'`);
+
+        // completing the message will remove it from the remote queue or subscription.
+        await queueReceiver.completeMessage(message);
+      }
     }
 
-    return allMessages;
+    await queueReceiver.close();
+  } finally {
+    await sbClient.close();
+  }
+
+  return allMessages;
 }
 
-async function elasticDemo() {
-    const client = new Client({
+async function sendDataToElastic(data) {
+  const indexName = "energy_price";
+  
+  const client = new Client({
         node: 'https://openelastic.es.eastus2.azure.elastic-cloud.com:9243',
         auth: {
           username: 'elastic',
@@ -54,52 +56,31 @@ async function elasticDemo() {
         }
     });
 
-    await client.indices.create({
-        index: 'tweets',
-        operations: {
-          mappings: {
-            properties: {
-              id: { type: 'integer' },
-              text: { type: 'text' },
-              user: { type: 'keyword' },
-              time: { type: 'date' }
-            }
-          }
+  await client.indices.create({
+    index: indexName,
+    operations: {
+      mappings: {
+        properties: {
+          price: { type: 'double' },
+          date: { type: 'date' }
         }
-      }, { ignore: [400] })
+      }
+    }
+  }, { ignore: [400] })
     
-      const dataset = [{
-        id: 1,
-        text: 'If I fall, don\'t bring me back.',
-        user: 'jon',
-        date: new Date()
-      }, {
-        id: 2,
-        text: 'Winter is coming',
-        user: 'ned',
-        date: new Date()
-      }, {
-        id: 3,
-        text: 'A Lannister always pays his debts.',
-        user: 'tyrion',
-        date: new Date()
-      }, {
-        id: 4,
-        text: 'I am the blood of the dragon.',
-        user: 'daenerys',
-        date: new Date()
-      }, {
-        id: 5, // change this value to a string to see the bulk response with errors
-        text: 'A girl is Arya Stark of Winterfell. And I\'m going home.',
-        user: 'arya',
-        date: new Date()
-      }]
-    
-      const operations = dataset.flatMap(doc => [{ index: { _index: 'tweets' } }, doc])
-    
-      const bulkResponse = await client.bulk({ refresh: true, operations })
-      
-      console.log(JSON.stringify(bulkResponse));
+  const operations = data.flatMap(doc => [{ index: { _index: indexName } }, doc])
+
+  const bulkResponse = await client.bulk({ refresh: true, operations })
+  
+  console.log(JSON.stringify(bulkResponse));
+}
+
+function messageToPriceList(messages) {
+  if(messages === undefined || messages.length === 0) {
+    return [];
+  }
+
+  // return messages.map(m => {"price": m.price, "date": m.date});
 }
 
 async function main() {
@@ -108,11 +89,23 @@ async function main() {
     // const messages = await receiveMessages(1);
     
     // Send data to Elastic
-    await elasticDemo();
+
+  const dataset = [
+    {
+      price: 1.2,
+      date: "2015-01-01T12:00:00Z",
+    },
+    {
+      price: 1.3,
+      date: "2015-01-01T13:00:00Z",
+    } 
+  ];
+
+  await sendDataToElastic(dataset);
 }
 
 main().catch((err) => {
-    console.log("ReceiveMessageLoop Sample - Error occurred: ", err);
-    process.exit(1);
+  console.log("Data Sender - Error occurred: ", err);
+  process.exit(1);
 });
 
