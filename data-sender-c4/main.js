def3c92b8bd2cc9909441585ab8ed09f4b266224
Dany Manastireanu <danym@microsoft.com>
2022-12-01 17:40:10 +0200

    Added demo elastic

--- data-sender-c4/main.js
+++ data-sender-c4/main.js
@@ -45,13 +45,70 @@ async function receiveMessages(numberOfMessages) {
     return allMessages;
 }
 
+async function elasticDemo() {
+    const client = new Client({
+        node: 'https://openelastic.es.eastus2.azure.elastic-cloud.com:9243',
+        auth: {
+          username: 'elastic',
+          password: 'YvptJOOKMpzZS6ryly54AQpv'
+        }
+    });
+
+    await client.indices.create({
+        index: 'tweets',
+        operations: {
+          mappings: {
+            properties: {
+              id: { type: 'integer' },
+              text: { type: 'text' },
+              user: { type: 'keyword' },
+              time: { type: 'date' }
+            }
+          }
+        }
+      }, { ignore: [400] })
+    
+      const dataset = [{
+        id: 1,
+        text: 'If I fall, don\'t bring me back.',
+        user: 'jon',
+        date: new Date()
+      }, {
+        id: 2,
+        text: 'Winter is coming',
+        user: 'ned',
+        date: new Date()
+      }, {
+        id: 3,
+        text: 'A Lannister always pays his debts.',
+        user: 'tyrion',
+        date: new Date()
+      }, {
+        id: 4,
+        text: 'I am the blood of the dragon.',
+        user: 'daenerys',
+        date: new Date()
+      }, {
+        id: 5, // change this value to a string to see the bulk response with errors
+        text: 'A girl is Arya Stark of Winterfell. And I\'m going home.',
+        user: 'arya',
+        date: new Date()
+      }]
+    
+      const operations = dataset.flatMap(doc => [{ index: { _index: 'tweets' } }, doc])
+    
+      const bulkResponse = await client.bulk({ refresh: true, operations })
+      
+      console.log(JSON.stringify(bulkResponse));
+}
+
 async function main() {
     // Receice message from ServiceBus
 
-    const messages = await receiveMessages(1);
+    // const messages = await receiveMessages(1);
     
     // Send data to Elastic
-
+    await elasticDemo();
 }
 
 main().catch((err) => {
